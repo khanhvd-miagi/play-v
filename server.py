56e6f01c63030b05057b3d58d0a026229e9062dd
khanhvd <khanhvd@miagi-so.net>
2022-04-07 16:17:53 +0700

    up

--- server.py
+++ server.py
@@ -22,9 +22,6 @@ def hello_world(request):
     input_key = '20220406/mytam.mp4'
     
     # HLS Presets that will be used to create an adaptive bitrate playlist.
-    hls_64k_audio_preset_id = '1351620000001-200071';
-    hls_0400k_preset_id     = '1351620000001-200050';
-    hls_0600k_preset_id     = '1351620000001-200040';
     hls_1000k_preset_id     = '1351620000001-200030';
     hls_1500k_preset_id     = '1351620000001-200020';
     hls_2000k_preset_id     = '1351620000001-200010';
@@ -37,43 +34,31 @@ def hello_world(request):
     
     # Setup the job outputs using the HLS presets.
     output_key = hashlib.sha256(input_key.encode('utf-8')).hexdigest()
-    hls_audio = {
-        'Key' : 'hlsAudio/' + output_key,
-        'PresetId' : hls_64k_audio_preset_id,
-        'SegmentDuration' : segment_duration
-    }
-    hls_400k = {
-        'Key' : 'hls0400k/' + output_key,
-        'PresetId' : hls_0400k_preset_id,
-        'SegmentDuration' : segment_duration
-    }
-    hls_600k = {
-        'Key' : 'hls0600k/' + output_key,
-        'PresetId' : hls_0600k_preset_id,
-        'SegmentDuration' : segment_duration
-    }
     hls_1000k = {
         'Key' : 'hls1000k/' + output_key,
         'PresetId' : hls_1000k_preset_id,
+        # 'ThumbnailPattern': 'hls1000k/thumbnail/' + output_key + '-{resolution}-{count}',
         'SegmentDuration' : segment_duration
     }
     hls_1500k = {
         'Key' : 'hls1500k/' + output_key,
         'PresetId' : hls_1500k_preset_id,
+        # 'ThumbnailPattern': 'hls1500k/thumbnail/' + output_key + '-{resolution}-{count}',
         'SegmentDuration' : segment_duration
     }
     hls_2000k = {
         'Key' : 'hls2000k/' + output_key,
         'PresetId' : hls_2000k_preset_id,
+        # 'ThumbnailPattern': 'hls2000k/thumbnail/' + output_key + '-{resolution}-{count}',
         'SegmentDuration' : segment_duration
     }
-    job_outputs = [ hls_audio, hls_400k, hls_600k, hls_1000k, hls_1500k, hls_2000k ]
+    job_outputs = [ hls_1000k, hls_1500k, hls_2000k ]
     playlist = [
         {
             'Name' : 'hls_' + output_key,
             'Format' : 'HLSv3',
             'OutputKeys' : [
-                hls_audio['Key'], hls_400k['Key'], hls_600k['Key'], hls_1000k['Key'], hls_1500k['Key'], hls_2000k['Key']
+                hls_1000k['Key'], hls_1500k['Key'], hls_2000k['Key']
             ],
             'HlsContentProtection': {
                 'Method': 'aes-128',
@@ -93,8 +78,10 @@ def hello_world(request):
     create_job_result=client.create_job(PipelineId=pipeline_id,
                                         Input=job_input,
                                         Outputs=job_outputs,
-                                        OutputKeyPrefix=output_key_prefix + output_key +'/',
-                                        Playlists=playlist)
+                                        OutputKeyPrefix=output_key_prefix + output_key + '/',
+                                        Playlists=playlist,
+                                        UserMetadata={}
+                                        )
     
     return Response(create_job_result["Id"])
 
