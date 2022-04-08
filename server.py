8464ed3896ca272ec969081556d2cf3dd92a6239
khanhvd <khanhvd@miagi-so.net>
2022-04-08 10:06:40 +0700

    up

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 from wsgiref.simple_server import make_server
-
 import os
 import hashlib
-
+from datetime import datetime
 import boto3
 
 def main():
     # Region where the sample will be run
     region = 'us-east-1'
     client = boto3.client('elastictranscoder', 
-                          aws_access_key_id='ASIARWWPZA7V6PCGEH4H',
-                          aws_secret_access_key='JcwghLVMV8mQIAR6pkmCcwdVfW9eWpiLGVgkn55T',
-                          aws_session_token='IQoJb3JpZ2luX2VjEDMaCXVzLWVhc3QtMSJIMEYCIQCt4WrjhHARmkOlggpDDAgQbqLc2BooI0mHcz9RhJX9JgIhAJ24oHgkcw4rJ2bFS9yjF703iNC2VwdAYrawr7icq7WqKtwBCMz//////////wEQABoMMTE3NDczNjA5NzA3IgyxhLe3FkfQEFy6BRQqsAFFG7eLKGfr0y3yIzahN29OgemLqeyeUSwGrp4YZ4Apktbnzn7cCstkpkXQQJKuGca0jKRYDg+aVCirT+8LWb2KuM2hlDTpRShUtwKKPVnFyDBRKrpXVJFSpmMlYUkJsgoy/IV9ZpZ1g0t+aoJ07uE8mkR9Wo6U0kC0giZbLDzVwjQ5bZjCJbMFh4jy3NLpz6vuOncarJepAqJg6+79p3COEmc/oBmUnB6IzR6nS8YVgzDHur6SBjqXAdGptzN7+l6KiXV+iD1cgFVrF7LbySydfTNyrqPH/7S3Cdk2CioLKjzw8GN69jBoYW9HPRc69c6hroDsgXky9J34G6yp4wQqZkMg6Eyxqio5JvyvQfMkxDsjJEpQMNtkQVhXyQxp1MYiZqilTlt6sSyHYsANs/fNX0Mw/vluj5B/pd6Y6fRWSqNxvkg7F/wZoFzTJLssCjk=',
+                          aws_access_key_id='ASIARWWPZA7VSRLON3HK',
+                          aws_secret_access_key='Iw3ScfgGDOZmXCm3Gbg9zFwPe0+H4SM4/LIto4+s',
+                          aws_session_token='IQoJb3JpZ2luX2VjEDMaCXVzLWVhc3QtMSJHMEUCIQDjwWwOzS33bSqLUMYw0mVHU8VYCHBuEU1xbUVgquHbswIgcDwxqazBSz4Y4ZJvj0CXc6ia/jy3NGoU4Wla0G1cxWoq3AEIzP//////////ARAAGgwxMTc0NzM2MDk3MDciDD4AAF+7KhDY/he0lSqwAR+F9C8l3qlxPObHv+E7SSBNO5ou+9lZud1E9O8aBJuC2byOvIjCL+K0Ux4P5sXPpKSIKq2CFFhxgsv9Mi8BTETBYf6YchywcliYwBSyAVRNFvQ3LxivYb1mIWtbD96gqIcmRwqYQcb2MxQOR2Btia7U4epSGHDn/zSns0482TS57iUcftrfmMu5bXPFsiMJzHYMihH737bHw60ATERNrUnmj7DHOQWbIvrMrgUJQ9uYMO7JvpIGOpgBZ/avq2PyqwQFGQSq1QdlPe67kPqKNwJYASbsnMCgUbK5dIIrWQy8U6gSZYHX5BH0tSvmQJQcj9DqGUqTVqMhCw0rXXtjAKTe+GnFTRbHz/f2oBHioBrAZu07CkWxOQSsnx6JdoQ2fPkCsFRHv6NBerbWE70G3h9WS1bW0Yan14NGOanTtKbX+bZZUVZbNgQCsuxXqKX2GkU=',
                           region_name=region
                           )
     
@@ -23,7 +22,7 @@ def main():
     input_key = 'mytam.mp4'
     
     # Presets System preset: Generic 480p 16:9.
-    mp4_480p = '1351620000001-000020'
+    mp4_1080p = '1351620000001-000001'
 
     #All outputs will have this prefix prepended to their output key.
     output_key_prefix = 'output/'
@@ -31,23 +30,20 @@ def main():
     job_input = { 'Key': input_key }
     
     # Setup the job outputs using the presets.
-    output_key = hashlib.sha256(input_key.encode('utf-8')).hexdigest()
+    now = datetime.now() # current date and time
+    output_key = now.strftime("%Y%m%d%H%M%S")
 
-
-    mp4_480p = {
+    job_outputs = [{
         'Key' : 'Output/' + output_key + '.mp4',
-        'PresetId' : mp4_480p,
+        'PresetId' : mp4_1080p,
         'ThumbnailPattern': '',
-    }
-    job_outputs = [ mp4_480p ]
+    }]
 
     # Creating the job.
     create_job_request = {
         'PipelineId' : pipeline_id,
         'Input' : job_input,
-        'Outputs' : job_outputs,
-        # 'OutputKeyPrefix' : output_prefix,
-        # 'Playlists' : playlist
+        'Outputs' : job_outputs
     }
     
     client.create_job(**create_job_request)
